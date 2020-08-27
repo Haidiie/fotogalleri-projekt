06a9498a7bd644135f01e70d05b264453a73c946
HeidarSelman <heidar.selman@hotmail.com>
2020-08-27 11:34:02 +0200

    startup

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Fotogalleri</h1>
+      <h2>Dina Bilder</h2>
+      <p>Ladda upp dina favorit foton</p>
+    </div>
+  )
+}
+
+export default Title;
